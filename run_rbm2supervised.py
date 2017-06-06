8c51368be4f71558cb18c18c1b3a7f1f7664d5ba
lizeyan <zy-li14@hotmail.com>
2017-06-06 16:10:13 +0800

    update params

--- run_rbm2supervised.py
+++ run_rbm2supervised.py
@@ -43,8 +43,8 @@ def main():
     # CNN
     ####################################################################################################################
     # cnn = keras.models.Sequential()
-    # cnn.add(keras.layers.Conv2D(32, kernel_size=(3, 3), activation="relu", input_shape=IMAGE_SIZE + (1, )))
-    # cnn.add(keras.layers.Conv2D(64, kernel_size=(3, 3), activation="relu"))
+    # cnn.add(keras.layers.Conv2D(128, kernel_size=(3, 3), activation="relu", input_shape=IMAGE_SIZE + (1, )))
+    # cnn.add(keras.layers.Conv2D(256, kernel_size=(3, 3), activation="relu"))
     # cnn.add(keras.layers.MaxPool2D(pool_size=(2, 2)))
     # cnn.add(keras.layers.Dropout(0.25))
     # cnn.add(keras.layers.Flatten())
@@ -52,8 +52,9 @@ def main():
     # cnn.add(keras.layers.Dropout(0.5))
     # cnn.add(keras.layers.Dense(10, activation="softmax"))
     # cnn.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(), metrics=["accuracy"])
-    # cnn.fit(expanded_train_data, one_hot_train_label, batch_size=100, epochs=20, verbose=True, validation_data=(expanded_test_data, one_hot_test_label))
+    # cnn.fit(expanded_train_data, one_hot_train_label, batch_size=32, epochs=100, verbose=True, validation_data=(expanded_test_data, one_hot_test_label))
     # evaluate(cnn.predict(expanded_test_data), one_hot_test_label, "CNN")
+    # return
 
     ####################################################################################################################
     # MLP
@@ -61,36 +62,34 @@ def main():
     # mlp = keras.models.Sequential()
     # mlp.add(keras.layers.Dropout(0.25, input_shape=IMAGE_SIZE + (1, )))
     # mlp.add(keras.layers.Flatten())
-    # mlp.add(keras.layers.Dense(1024, activation="relu"))
     # mlp.add(keras.layers.Dense(512, activation="relu"))
+    # mlp.add(keras.layers.Dense(256, activation="relu"))
     # mlp.add(keras.layers.Dropout(0.5))
     # mlp.add(keras.layers.Dense(10, activation="softmax"))
     # mlp.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(lr=0.001), metrics=["accuracy"])
-    # mlp.fit(expanded_train_data, one_hot_train_label, batch_size=512, epochs=1000, verbose=True, validation_data=(expanded_test_data, one_hot_test_label))
+    # mlp.fit(expanded_train_data, one_hot_train_label, batch_size=32, epochs=1000, verbose=True, validation_data=(expanded_test_data, one_hot_test_label))
     # evaluate(mlp.predict(expanded_test_data), one_hot_test_label, "MLP")
 
     ####################################################################################################################
     # Simple and Direct Models
     ####################################################################################################################
-    # evaluate(fit_and_predict(linear_model.LogisticRegression, flatten_train_data, train_label, flatten_test_data, C=100.0), test_label, "Logistic Regression", )
+    evaluate(fit_and_predict(linear_model.LogisticRegression, flatten_train_data, train_label, flatten_test_data, C=100.0), test_label, "Logistic Regression", )
 
-    # evaluate(fit_and_predict(linear_model.PassiveAggressiveClassifier(C=0.8), flatten_train_data, train_label, flatten_test_data), test_label, "Passive Aggressor Classifier", )
+    evaluate(fit_and_predict(linear_model.PassiveAggressiveClassifier, flatten_train_data, train_label, flatten_test_data), test_label, "Passive Aggressor Classifier", C=0.8)
 
-    # evaluate(fit_and_predict(svm.LinearSVR(C=1.0, epsilon=0), flatten_train_data, train_label, flatten_test_data), test_label, "linear SVR", )
+    evaluate(fit_and_predict(svm.LinearSVR, flatten_train_data, train_label, flatten_test_data), test_label, "linear SVR", C=1.0, epsilon=0)
 
-    # evaluate(fit_and_predict(ensemble.RandomForestClassifier(n_jobs=4, n_estimators=100, max_depth=38), flatten_train_data, train_label, flatten_test_data), test_label, "random forest", )
+    evaluate(fit_and_predict(ensemble.RandomForestClassifier, flatten_train_data, train_label, flatten_test_data), test_label, "random forest", n_jobs=20, n_estimators=100, max_depth=38)
 
-    # evaluate(fit_and_predict(tree.DecisionTreeClassifier(max_depth=37), flatten_train_data, train_label, flatten_test_data), test_label, "decision tree")
+    evaluate(fit_and_predict(tree.DecisionTreeClassifier, flatten_train_data, train_label, flatten_test_data), test_label, "decision tree", max_depth=37)
 
-    # evaluate(fit_and_predict(neural_network.MLPClassifier, flatten_train_data, train_label, flatten_test_data, hidden_layer_sizes=(512, 64), verbose=True, tol=1e-3, learning_rate_init=1e-3, shuffle=True, alpha=1e-3, batch_size=64), test_label, "Multi-Layer Perception", )
+    evaluate(fit_and_predict(neighbors.KNeighborsClassifier, flatten_train_data, train_label, flatten_test_data), test_label, "knn", n_neighbors=1)
 
-    # evaluate(fit_and_predict(neighbors.KNeighborsClassifier(n_neighbors=1), flatten_train_data, train_label, flatten_test_data), test_label, "knn")
+    evaluate(fit_and_predict(ensemble.AdaBoostClassifier, flatten_train_data, train_label, flatten_test_data), test_label, "adaboost", n_estimators=100)
 
-    # evaluate(fit_and_predict(ensemble.AdaBoostClassifier(n_estimators=100), flatten_train_data, train_label, flatten_test_data), test_label, "adaboost")
+    evaluate(fit_and_predict(linear_model.SGDClassifier, flatten_train_data, train_label, flatten_test_data), test_label, "SGD Classifier", n_iter=200, shuffle=True, n_jobs=20)
 
-    # evaluate(fit_and_predict(linear_model.SGDClassifier(n_iter=200, shuffle=True, n_jobs=4, warm_start=True), flatten_train_data, train_label, flatten_test_data), test_label, "SGD Classifier")
-
-    # evaluate(fit_and_predict(svm.SVC, flatten_train_data, train_label, flatten_test_data, C=8000.0, kernel="rbf", max_iter=-1, cache_size=4096), test_label, "SVC Classifier")
+    evaluate(fit_and_predict(svm.SVC, flatten_train_data, train_label, flatten_test_data, C=8000.0, kernel="rbf", max_iter=-1, cache_size=4096), test_label, "SVC Classifier")
 
     ####################################################################################################################
     # RBM -> Supervised Models
@@ -104,7 +103,7 @@ def main():
     # evaluate(fit_and_predict(svm.SVC, transformed_train_data, train_label, transformed_test_data, C=512000, cache_size=4096), test_label, "RBM-SVC Classifier")
     # evaluate(fit_and_predict(linear_model.LogisticRegression, transformed_train_data, train_label, transformed_test_data, C=3000.0), test_label, "RBM-Logistic Classifier")
 
-    transformed_size = (64, 64, 1)
+    transformed_size = (32 * 4, 32 * 4, 1)
     # rbm = neural_network.BernoulliRBM(n_components=transformed_size[0] * transformed_size[1], learning_rate=0.001, verbose=True, n_iter=10, random_state=0)
     rbm = BinaryRBM(n_hidden_units=transformed_size[0] * transformed_size[1], activation_function="relu", n_epochs=100, batch_size=32, optimization_algorithm="sgd", learning_rate=1e-3)
     rbm.fit(flatten_train_data)
@@ -115,17 +114,17 @@ def main():
     ####################################################################################################################
     # RBM->SVC
     ####################################################################################################################
-    # evaluate(fit_and_predict(svm.SVC, transformed_train_data, train_label, transformed_test_data, C=128000, cache_size=4096), test_label, "RBM-SVC Classifier")
+    evaluate(fit_and_predict(svm.SVC, transformed_train_data, train_label, transformed_test_data, C=128000, cache_size=4096), test_label, "RBM-SVC Classifier")
 
     ####################################################################################################################
     # RBM->Logistic
     ####################################################################################################################
-    evaluate(fit_and_predict(linear_model.LogisticRegression, transformed_train_data, train_label, transformed_test_data, C=2000.0), test_label, "RBM-Logistic Classifier")
+    evaluate(fit_and_predict(linear_model.LogisticRegression, transformed_train_data, train_label, transformed_test_data, C=400.0), test_label, "RBM-Logistic Classifier")
 
     ####################################################################################################################
     # RBM->RandomForest
     ####################################################################################################################
-    evaluate(fit_and_predict(ensemble.RandomForestClassifier, transformed_train_data, train_label, transformed_test_data, n_estimators=100, max_depth=32), test_label, "RBM-random Forest Classifier")
+    evaluate(fit_and_predict(ensemble.RandomForestClassifier, transformed_train_data, train_label, transformed_test_data, n_estimators=800, max_depth=32), test_label, "RBM-random Forest Classifier")
 
     transformed_train_data = transformed_train_data.reshape((-1, ) + transformed_size)
     transformed_test_data = transformed_test_data.reshape((-1, ) + transformed_size)
